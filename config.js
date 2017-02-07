14e95f0b794557aebf079f266ea384e23b114269
Stefan Buchholtz <stbuchholtz@gmail.com>
2017-02-07 16:36:56 +0100

    specify sensitive configuration settings through enviroment variables

--- config.js
+++ config.js
@@ -1,7 +1,20 @@
 /* jshint esversion: 6 */
-
 const environment = process.env.NODE_ENV || 'development';
-const config = require('./config/' + environment + '.json');
+
+var config = require('./config/' + environment + '.json');
+
+if (process.env.DB_USER) {
+	config.database.user = process.env.DB_USER;
+}
+if (process.env.DB_PASSWORD) {
+	config.database.password = process.env.DB_PASSWORD;
+}
+if (process.env.TWITCH_CLIENT_SECRET) {
+	config.twitch.client_secret = process.env.TWITCH_CLIENT_SECRET;
+}
+if (process.env.PUSHBULLET_CLIENT_SECRET) {
+	config.pushbullet.client_secret = process.env.PUSHBULLET_CLIENT_SECRET;
+}
 
 const protocol = config.app.protocol || 'http';
 const port = config.app.port ? ":" + config.app.port : "";
